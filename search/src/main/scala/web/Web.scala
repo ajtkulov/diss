2896fac2e6a75f5f57e22ce37fe2c3b7b9b9be5e
Pavel Ajtkulov <ajtkulov@gmail.com>
2021-12-27 18:41:02 +0400

    -

--- search/src/main/scala/web/Web.scala
+++ search/src/main/scala/web/Web.scala
@@ -1,15 +1,54 @@
 package web
 
-object Web extends cask.MainRoutes{
+object Web extends cask.MainRoutes {
   @cask.get("/")
   def hello() = {
     "Hello World!"
   }
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) = {
-    request.text().reverse
+  @cask.postJson("/search")
+  def doThing(data: ujson.Value) = {
+    val str = data.str
+
+    str
   }
 
   initialize()
+}
+
+object NumSearch {
+  val fileName = "diss.num.tt"
+
+  def normalize(str: String): List[String] = {
+    val list = str.filterNot(x => x == ',' || x == '.').map(x => if (x.isDigit) x else ' ').split(" ").filter(_.nonEmpty).toList
+    val res = list.filter { x =>
+      (x.length < 10 && ((x.toLong < 1900 && x.toLong > 10) || (x.toLong > 2030)) || x.startsWith("0"))
+    }.filterNot(x => x.forall(_ == '0'))
+    if (res.size <= 3) {
+      List()
+    } else {
+      res
+    }
+  }
+
+  def hash(str: String, seed: Int): Int = {
+    var res = 1
+    for (c <- str) {
+      res = res * seed + c.toInt
+    }
+
+    res
+  }
+
+
+  def find(value: String) = {
+    val numbers = normalize(value)
+
+    numbers.sliding(3, 1).map { triple =>
+      val str = triple.mkString(" ")
+      val lookFor = s"${hash(str, 5)}\t${hash(str, 7)}\t"
+
+      BinSearch.find(fileName, lookFor)
+    }
+  }
 }
